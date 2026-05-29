ffb0c4da8cf26fd74165d9869af7e3642f40002c
OGs-tradeathem <tradeathem@gmail.com>
2026-05-29 10:06:52 +0100

    Redesign live Men of Stone site

--- api/donate.js
+++ api/donate.js
@@ -1,5 +1,5 @@
 const fundraiserUrl =
-  "https://www.gofundme.com/f/a-farewell-for-phil-stone?attribution_id=sl:31c75a13-1790-4652-a3f5-86b54fec34ea&lang=en_GB&ts=1779568085&utm_campaign=pd_ss_icons&utm_content=amp20_control&utm_medium=customer&utm_source=facebook";
+  "https://www.gofundme.com/f/donate-in-memory-of-phil-stone?attribution_id=sl%3Aa049a459-4be5-4363-b083-1979c5566c78&lang=en_GB&ts=1779604869&utm_campaign=fp_sharesheet&utm_content=amp20_t1&utm_medium=customer&utm_source=copy_link&fbclid=IwY2xjawSGNVlleHRuA2FlbQIxMABicmlkETFHNENMYVVsUGZ2TFZFamdCc3J0YwZhcHBfaWQQMjIyMDM5MTc4ODIwMDg5MgABHgAHdok4IPNnlNmOjFonKvXZIJkLftoFTp6uajleuK8gK97kVjBeihvaqwOD_aem_qHyDX9gk7Ny4wmt8KqzgOQ&utm_id=97757_v0_s00_e227_tv0";
 
 module.exports = function handler(request, response) {
   const forwardedFor = request.headers["x-forwarded-for"] || "";
